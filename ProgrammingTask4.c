d9741872bf845efd00d220b6c0e298417eec00d0
karllogdat <logdatk@gmail.com>
2024-11-08 00:03:41 +0800

    Added traversal functions

--- ProgrammingTask4.c
+++ ProgrammingTask4.c
@@ -5,9 +5,70 @@
 // Karl Joseph Logdat
 
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Node* NewNode(int value) {
+    Node* node = (Node*) malloc(sizeof(Node));
+    node->value = value;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
+void InOrder(Node* node) {
+    if (node == NULL)
+        return;
+
+    InOrder(node->left);
+    printf("%d ",node->value);
+    InOrder(node->right);
+}
+
+void PreOrder(Node* node) {
+    if (node == NULL)
+        return;
+    
+    printf("%d ",node->value);
+    PreOrder(node->left);
+    PreOrder(node->right);
+}
+
+void PostOrder(Node* node) {
+    if (node == NULL)
+        return;
+    
+    PostOrder(node->left);
+    PostOrder(node->right);
+    printf("%d ", node->value);
+}
 
 int main(){
-    printf("This shit confusing");
+    // TEST CODE LANG TONG ASA MAIN
+    // Delete niyo nalang if ccode niyo na mismo
+    //      1
+    //     / \
+    //    2  3
+    //   / \ 
+    //  4  5
+    Node* root = NewNode(1);
+    root->left = NewNode(2);
+    root->right = NewNode(3);
+    root->left->left = NewNode(4);
+    root->left->right = NewNode(5);
+
+    InOrder(root);
+    printf("\n");
+    PreOrder(root);
+    printf("\n");
+    PostOrder(root);
+    printf("\n");
 
     return 0;
 }
