430890c3d1e1e288a30f1f34ce2700db9ea561dc
blackxero0920 <jankevingerona15@gmail.com>
2025-01-10 15:49:11 +0800

    Final Version

--- ProgrammingTask5.c
+++ ProgrammingTask5.c
@@ -24,10 +24,6 @@ void cleanString(char *string){
     string[j] = '\0';
 }
 
-void readFile(){
-    // file crud here
-}
-
 int search(char* arr, int n, char x) {
     for (int i = 0; i < n; i++)
         if (arr[i] == x)
@@ -54,16 +50,61 @@ void printPostOrder(char* in, char* pre, int inStart, int inEnd, int* preIndex, 
     printPostOrder(in, pre, rootIndex + 1, inEnd, preIndex, n);
 
     // Print the root (postorder)
-    printf("%c ", root);
+    printf("%c", root);
+}
+
+int countElements(char *string) {
+    int count = 0;
+    for (int i = 0; string[i] != '\0'; i++) {
+        count++; // Count each character until null terminator
+    }
+    return count;
+}
+
+void removeNewline(char *string) {
+    int length = strlen(string);
+    if (string[length - 1] == '\n') {
+        string[length - 1] = '\0';
+    }
+}
+
+void splitAndPrint(char *line){
+    char pre[50], in[50];
+    int count = countElements(line);
+    int mid = count / 2;
+
+    strncpy(pre, line, mid);
+    pre[mid] = '\0';
+    strncpy(in, line + mid, count - mid);
+    in[count - mid] = '\0';
+
+    int preIndex = 0;
+    printPostOrder(in, pre, 0, mid - 1, &preIndex, mid);
+    printf("\n");
+}
+
+void readFile() {
+    // All variables declaration for this function
+    FILE *file;
+    char currentLine[100];
+
+    file = fopen("input.txt", "r");
+    if (file == NULL) {
+        printf("Error: Could not open file.\n");
+        return;
+    }
+
+    // Reads line and split into two array for pre-order and in-order
+    while (fgets(currentLine, sizeof(currentLine), file)) {
+        removeNewline(currentLine);
+        cleanString(currentLine);
+        splitAndPrint(currentLine);
+    }
+
+    fclose(file);
 }
 
 int main() {
-    // TODO: Input should be from text file
-    char pre[] = {'D', 'B', 'A', 'C', 'E', 'G', 'F'};
-    char in[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
-    int n = sizeof(in) / sizeof(in[0]);
-    int preIndex = 0;
-
-    printPostOrder(in, pre, 0, n - 1, &preIndex, n);
+    readFile();
     return 0;
 }
