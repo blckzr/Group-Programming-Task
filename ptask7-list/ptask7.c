a5deb0cb1daaad07f17a642b46183fa685b799fc
karllogdat <logdatk@gmail.com>
2025-01-14 03:01:18 +0800

    idfk bruh 16 talaga output di 14

--- ptask7-list/ptask7.c
+++ ptask7-list/ptask7.c
@@ -1,18 +1,22 @@
 #include <limits.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// rename unsigned int to u32 for ease of typing
+typedef unsigned int u32;
+
 typedef struct Node
 {
-	unsigned int vertex;
+	u32 vertex;
 	struct Node *next;
 } Node;
 
 typedef struct Graph
 {
-	unsigned int size;
+	u32 size;
 	Node **adj_list;
 } Graph;
 
@@ -24,7 +28,7 @@ Graph *graph()
 	return g;
 }
 
-Node *node(unsigned int vertex)
+Node *node(u32 vertex)
 {
 	Node *n = malloc(sizeof(Node));
 	n->vertex = vertex;
@@ -57,7 +61,7 @@ void graph_insert_cnode(Graph *g)
 
 	g->adj_list[g->size - 1] = NULL;
 
-	for (unsigned int i = 0; i < g->size - 1; ++i)
+	for (u32 i = 0; i < g->size - 1; ++i)
 	{
 		Node *new_connection = node(g->size - 1);
 		new_connection->next = g->adj_list[i];
@@ -79,7 +83,7 @@ void free_adjacency_list_entry(Node *list)
 	}
 }
 
-void graph_delete_node_at(Graph *g, unsigned int index)
+void graph_delete_node_at(Graph *g, u32 index)
 {
 	// add proper checking g.size == 0 and g.size < index
 	if (g->size == 0)
@@ -96,7 +100,7 @@ void graph_delete_node_at(Graph *g, unsigned int index)
 
 	free_adjacency_list_entry(g->adj_list[index]);
 
-	for (unsigned int i = 0; i < g->size; i++)
+	for (u32 i = 0; i < g->size; i++)
 	{
 		if (i == index)
 			continue;
@@ -137,7 +141,7 @@ void graph_delete_node_at(Graph *g, unsigned int index)
 	}
 
 	// shift values starting at index
-	for (unsigned int i = index; i < g->size - 1; ++i)
+	for (u32 i = index; i < g->size - 1; ++i)
 	{
 		g->adj_list[i] = g->adj_list[i + 1];
 	}
@@ -153,7 +157,7 @@ void graph_delete_node_at(Graph *g, unsigned int index)
 	g->size--;
 }
 
-unsigned int graph_get_connections_of_node(Graph *g, unsigned int index)
+u32 graph_get_connections_of_node(Graph *g, u32 index)
 {
 	if (g == NULL || g->adj_list == NULL)
 	{
@@ -167,7 +171,7 @@ unsigned int graph_get_connections_of_node(Graph *g, unsigned int index)
 		return 0;
 	}
 
-	unsigned int connections = 0;
+	u32 connections = 0;
 
 	Node *temp = g->adj_list[index];
 	while (temp != NULL)
@@ -179,27 +183,102 @@ unsigned int graph_get_connections_of_node(Graph *g, unsigned int index)
 	return connections;
 }
 
-int main(void)
+/**
+ * Function implementing task requirements. Accepts
+ * `u32` as arguments (as indicated by the task)
+ * and performs the following directives based on
+ * array contents.
+ *
+ * Directives:
+ * 	`0`: Add disconnected node
+ * 	`1`: Add new node, connected to every existing node
+ *  `m > 1`: Remove all nodes whose degree is divisible by `m`.
+ * 	     Since `0` is divisible by all `m`, all disconnected
+ * 	     nodes must be removed.
+ */
+u32 ptask_7(u32 directives[], u32 size)
 {
 	Graph *g = graph();
 
-	graph_insert_dnode(g);
-	graph_insert_cnode(g);
-	graph_insert_dnode(g);
-	graph_insert_cnode(g);
-	graph_insert_dnode(g);
-	graph_insert_cnode(g);
-
-	for (unsigned int i = 0; i < g->size; ++i)
+	for (u32 i = 0; i < size; ++i)
 	{
-		if (graph_get_connections_of_node(g, i) % 3)
+		printf("\nProcessing directive %u: %u\n", i, directives[i]);
+
+		if (directives[i] == 0)
 		{
-			graph_delete_node_at(g, i);
+			graph_insert_dnode(g);
+			printf("Added disconnected node. Graph size: %u\n", g->size);
+			// Print degrees of all nodes
+			for (u32 k = 0; k < g->size; k++)
+			{
+				printf("Node %u degree: %u\n", k, graph_get_connections_of_node(g, k));
+			}
+		}
+		else if (directives[i] == 1)
+		{
+			graph_insert_cnode(g);
+			printf("Added connected node. Graph size: %u\n", g->size);
+			// Print degrees of all nodes
+			for (u32 k = 0; k < g->size; k++)
+			{
+				printf("Node %u degree: %u\n", k, graph_get_connections_of_node(g, k));
+			}
+		}
+		else
+		{
+			while (true)
+			{
+				bool any_deleted = false;
+				// First find nodes with degree 0
+				for (int j = 0; j < g->size; j++)
+				{
+					u32 degree = graph_get_connections_of_node(g, j);
+					if (degree == 0)
+					{ // Always delete disconnected nodes first
+						graph_delete_node_at(g, j);
+						any_deleted = true;
+						break;
+					}
+				}
+				if (any_deleted)
+					continue;
+
+				// Then find nodes with degree divisible by m
+				for (int j = 0; j < g->size; j++)
+				{
+					u32 degree = graph_get_connections_of_node(g, j);
+					if (degree != 0 && degree % directives[i] == 0)
+					{
+						graph_delete_node_at(g, j);
+						any_deleted = true;
+						break;
+					}
+				}
+				if (!any_deleted)
+					break;
+			}
 		}
 	}
 
-	fprintf(stdout, "Remaining nodes: %u\n", g->size);
+	printf("Final graph size: %u\n", g->size);
+	printf("Final node degrees:\n");
+	for (u32 k = 0; k < g->size; k++)
+	{
+		printf("Node %u degree: %u\n", k, graph_get_connections_of_node(g, k));
+	}
+	return g->size;
+}
 
-	printf("Hello world what the fuck\n");
+int main(void)
+{
+	u32 directives[] = {0, 0, 1, 1, 0, 0, 1, 1, 2, 5, 7, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 8};
+	u32 directives_size = 27;
+
+	u32 remaining = ptask_7(directives, directives_size);
+
+	printf(
+	    "Remaining nodes in the graph: %u\n",
+	    remaining);
+
 	return 0;
 }
